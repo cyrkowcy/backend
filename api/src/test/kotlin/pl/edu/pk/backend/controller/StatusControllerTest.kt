ce65cc39f724ba3a6bb845cb1d3b7c200536e06d
kotcrab <kotcrab@gmail.com>
2020-03-22 15:23:58 +0100

    Fix

--- api/src/test/kotlin/pl/edu/pk/backend/controller/StatusControllerTest.kt
+++ api/src/test/kotlin/pl/edu/pk/backend/controller/StatusControllerTest.kt
@@ -1,17 +1,20 @@
 package pl.edu.pk.backend.controller
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.vertx.core.Future
 import io.vertx.ext.web.RoutingContext
 import org.junit.jupiter.api.Test
+import pl.edu.pk.backend.model.ApiStatus
 import pl.edu.pk.backend.service.StatusService
-
 
 class StatusControllerTest {
   @Test
   fun `returns status`() {
     val context = mockk<RoutingContext>(relaxed = true)
     val service = mockk<StatusService>(relaxed = true)
+    every { service.getStatus() } returns Future.succeededFuture(ApiStatus())
     val controller = StatusController(service)
 
     controller.getStatus(context)
