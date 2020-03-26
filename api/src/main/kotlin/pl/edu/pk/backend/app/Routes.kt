ae1b1b176e319ee9df8b78caf1baf9677670dcbd
kotcrab <kotcrab@gmail.com>
2020-03-26 22:06:40 +0100

    Add cors handler, #15

--- api/src/main/kotlin/pl/edu/pk/backend/app/Routes.kt
+++ api/src/main/kotlin/pl/edu/pk/backend/app/Routes.kt
@@ -1,8 +1,12 @@
 package pl.edu.pk.backend.app
 
+import io.vertx.core.Handler
 import io.vertx.core.Vertx
+import io.vertx.core.http.HttpMethod
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.handler.CorsHandler
 import io.vertx.ext.web.handler.StaticHandler
 
 fun createRouter(vertx: Vertx, controllers: Controllers): Router {
@@ -10,6 +14,7 @@ fun createRouter(vertx: Vertx, controllers: Controllers): Router {
 
   router.route().handler(AccessLogger()::handle)
   router.route().handler(BodyHandler.create())
+  router.route().handler(createCorsHandler())
 
   with(controllers.statusController) {
     router.get("/status").handler(::getStatus)
@@ -24,4 +29,19 @@ fun createRouter(vertx: Vertx, controllers: Controllers): Router {
   router.route().handler(StaticHandler.create().setCachingEnabled(false))
 
   return router
+}
+
+private fun createCorsHandler(): Handler<RoutingContext>? {
+  return CorsHandler.create(".*.")
+    .allowedMethod(HttpMethod.GET)
+    .allowedMethod(HttpMethod.POST)
+    .allowedMethod(HttpMethod.PATCH)
+    .allowedMethod(HttpMethod.DELETE)
+    .allowedMethod(HttpMethod.OPTIONS)
+    .allowedHeader("Access-Control-Allow-Method")
+    .allowedHeader("Access-Control-Allow-Origin")
+    .allowedHeader("Access-Control-Allow-Credentials")
+    .allowedHeader("Content-Type")
+    .allowedHeader("Authorization")
+    .allowCredentials(true)
 }
