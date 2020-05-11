19184be154dd3221681351dfa5816d7f31536355
Piwko <marcin.piwko@comarch.com>
2020-05-11 22:51:02 +0200

    Refactor

--- core/src/main/kotlin/pl/edu/pk/backend/service/UserService.kt
+++ core/src/main/kotlin/pl/edu/pk/backend/service/UserService.kt
@@ -173,7 +173,7 @@ class UserService(
   }
 
   fun getAvailableTrips(description: String): Future<List<TripDto>> {
-    return tripRepository.getAvailableTrips(if (description == "null") "" else description)
+    return tripRepository.getAvailableTrips(description)
       .map { it.map { TripDto.from(it) } }
   }
 
